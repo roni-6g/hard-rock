180431ebf19a6cde45907c09fa5ba87e7105e2b1
roni-6g <mdroni710411@gmail.com>
2021-02-10 14:26:24 +0600

    Development done

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-function searchSongs() {
+// Call To API with Asynic Way
+// const searchSongs = async () => {
+//     const songInput = document.getElementById('search-song').value;
+//     const searchUrl = `https://api.lyrics.ovh/suggest/${songInput}`;
+//     const response = await fetch(searchUrl)
+//     const data = await response.json();
+//     songsResult(data.data);
+// }
+
+const searchSongs = async () => {
     const songInput = document.getElementById('search-song').value;
     const searchUrl = `https://api.lyrics.ovh/suggest/${songInput}`;
-    
     fetch(searchUrl)
-    .then(response => response.json())
-    .then(data => songsResult(data.data));
+        .then(res => res.json())
+        .then(data => songsResult(data.data))
+        .catch(error => errorResult("Something Went wrong, Try AGain Later"));
 }
 
 const songsResult = (songs) => {
@@ -32,14 +41,30 @@ const songsResult = (songs) => {
     });
 }
 
-const getLyrics = (artist,title) => {
+const getLyrics = async (artist, title) => {
     const lyricsUrl = `https://api.lyrics.ovh/v1/${artist}/${title}`;
-    fetch(lyricsUrl)
-    .then(res => res.json())
-    .then(data => displayLyric(data.lyrics));
+
+    try {
+        const res = await fetch(lyricsUrl);
+        const data = await res.json();
+        displayLyric(data.lyrics);
+
+    } catch (error) {
+        errorResult("Sorry, I can't load full song Lyric Now ! Try Again later !!");
+    }
+
 }
 
 const displayLyric = lyric => {
     const lyricsDiv = document.getElementById('full-lyric');
     lyricsDiv.innerText = lyric;
+}
+// Display Error Result
+const errorResult = error => {
+    const displayError = document.getElementById('error-result');
+    const errorDiv = document.createElement("div");
+    errorDiv.className = "alert alert-warning col-6 alert-custom";
+    errorDiv.innerText = error;
+    displayError.appendChild(errorDiv);
+    console.log(error);
 }
